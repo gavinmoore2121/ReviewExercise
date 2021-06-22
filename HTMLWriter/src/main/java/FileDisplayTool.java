9c1adb7548282b43fd2dcd346a07b4b8a88eee9a
gavinmoore2121 <76663367+gavinmoore2121@users.noreply.github.com>
2021-06-22 09:53:11 -0400

    Base Implementation

---
+++ HTMLWriter/src/main/java/FileDisplayTool.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FileDisplayTool extends HttpServlet {
+
+	private static final long serialVersionUID = -2323752342714953173L;
+	
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html"); // Set the content type
+		PrintWriter pw = response.getWriter(); // Get stream to write data
+		System.out.println("here");
+		
+		//BufferedReader reader = new BufferedReader(new FileReader("Fitness.txt"));
+		
+		//reader.lines().forEach(e -> pw.println(e));
+		
+		//reader.close();
+		
+		pw.close();
+	}
+
+	// Reroute to doGet.
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+	
+}
